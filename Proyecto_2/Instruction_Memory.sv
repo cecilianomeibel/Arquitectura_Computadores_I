43b8525143f7ada5e22536e0fbe83084375d2f7e
LudwinR <lujorabri@gmail.com>
2024-10-13 19:39:04 -0600

    Actualización de la carga de las instrucciones a ROM

--- Proyecto_2/Instruction_Memory.sv
+++ Proyecto_2/Instruction_Memory.sv
@@ -3,16 +3,15 @@ module Instruction_Memory(
 		 input logic [14:0] a,     //PCF
 		 output logic [19:0] rd   //instruccion
 );
-
-      reg [19:0] ROM [511:0];
-  
-      assign rd = (reset == 1'b0) ? {20{1'b0}} : ROM[a];   // Lee el dato en la direccion alineada "A"
-		                                                     // Se hace PC+1, porque las instrucciones son de 17 bits y es un número primo, no es divisible en bytes
-
-      initial begin
+	
+	logic [19:0] ROM [511:0];
+	
+	initial begin
 		  
-		  $readmemh("memfile.hex",ROM);
-		  
-      end
-
+	  $readmemb("memfile.hex",ROM);
+			  
+	end
+	
+	assign rd = (reset == 1'b0) ? {20{1'b0}} : ROM[a];   // Lee el dato en la direccion alineada "A"
+														 // Se hace PC+1, porque las instrucciones son de 17 bits y es un número primo, no es divisible en bytes
 endmodule
